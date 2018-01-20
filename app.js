366135a66074435585dfd0cf8b9189c71b725343
Kristian Gavran <kdev@kriga.net>
2018-01-20 15:14:36 +0100

    adding carCollection structure and outputting as JSON on GET request

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 let express = require('express');
 let app = express();
 
+let carCollection = new Array();
+carCollection.push({name:"Audi", hp: 200});
+carCollection.push({name:"BMW", hp: 220});
+console.log(carCollection);
+
 app.get("/", (req, res) => {
-    res.send("GET");
+    res.send(JSON.stringify(carCollection));
 });
 
 app.listen(3000, (param) => {
     console.log("App started at port 3000!");
 })
-
