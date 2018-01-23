5e5809523fdcc171a95b8f594897e07a11b167a3
Kristian Gavran <kdev@kriga.net>
2018-01-23 19:19:43 +0100

    changing GET endpoint to '/car' instead '/', adding POST endpoint with respective array extension

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 let express = require('express');
 let app = express();
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 let carCollection = new Array();
 carCollection.push({name:"Audi", hp: 200});
 carCollection.push({name:"BMW", hp: 220});
 console.log(carCollection);
 
-app.get("/", (req, res) => {
+app.get("/car", (req, res) => {
+    res.send(JSON.stringify(carCollection));
+});
+
+app.post("/car", (req, res) => {
+    carCollection.push(req.body);
     res.send(JSON.stringify(carCollection));
 });
 
