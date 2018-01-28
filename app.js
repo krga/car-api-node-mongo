1123b4961daf9b0b3b2b6e4cad8a80dc10bc7753
Kristian Gavran <kdev@kriga.net>
2018-01-28 12:22:56 +0100

    adding GET endpoint for detail information of a specific car in carCollection. commenting each endpoint

--- app.js
+++ app.js
@@ -6,22 +6,35 @@ app.use(express.urlencoded());
 
 let carCollection = new Array();
 let carIdCounter = 1;
-//carCollection.push({name:"Audi", hp: 200});
-//carCollection.push({name:"BMW", hp: 220});
 
-console.log(carCollection);
-
+// GET all cars in carCollection
 app.get("/car", (req, res) => {
     res.send(JSON.stringify(carCollection));
 });
 
+// insert a new car into the carCollection
 app.post("/car", (req, res) => {
     carToAdd = req.body;
     carToAdd.id=carIdCounter++;
-
     carCollection.push(carToAdd);
-    
+
     res.send(JSON.stringify(carCollection));
+});
+
+// GET a specific car in carCollection by the id
+app.get("/car/:id", (req, res) => {
+
+    /* find a car with the specific id which has to be
+    converted to Int to be able to match the find criteria */
+    singleCar = carCollection.find(x => x.id === parseInt(req.params.id));
+    console.log(singleCar);
+
+    // send a 404 error Msg. if no such car in the carCollection
+    if (typeof singleCar === 'undefined') {
+        res.status(404).send('Not found');
+    } else {
+        res.send(JSON.stringify(singleCar));
+    }
 });
 
 app.listen(3000, (param) => {
