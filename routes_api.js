e31acb8ae1642d317d9d316c28302f60d859a9ab
Kristian Gavran <krga@users.noreply.github.com>
2018-03-20 19:46:21 +0100

    deleting remaining old "/car" endpoints. Implementing DELETE & PUT for endpoint "/api/cars"

--- routes_api.js
+++ routes_api.js
@@ -35,22 +35,8 @@ router.post("/api/cars", (req, res) => {
     res.status(200).send('Car added.');
 });
 
-// GET all cars in carCollection
-router.get("/car", (req, res) => {
-    res.send(JSON.stringify(carCollection));
-});
-
-// insert a new car into the carCollection
-router.post("/car", (req, res) => {
-    carToAdd = req.body;
-    carToAdd.id=carIdCounter++;
-    carCollection.push(carToAdd);
-
-    res.status(200).send('Car added.');
-});
-
 // GET a specific car in carCollection by the id
-router.get("/car/:id", (req, res) => {
+router.get("/api/cars/:id", (req, res) => {
 
     /* find a car with the specific id which has to be
     converted to Int to be able to match the find criteria */
@@ -58,35 +44,44 @@ router.get("/car/:id", (req, res) => {
 
     // send a 404 error Msg. if no such car in the carCollection
     if (typeof singleCar === 'undefined') {
-        res.status(404).send('Not found');
+        res.status(404).send('Not found!');
     } else {
-        res.send(JSON.stringify(singleCar));
-    }
+        let dataJSON  = {data: singleCar};
+        res.send(JSON.stringify(dataJSON));    }
 });
 
-router.delete("/car/:id", (req, res) => {
+router.delete("/api/cars/:id", (req, res) => {
     let carToDeleteId = parseInt(req.params.id);
+    let carFound = false;
 
     for (let i = 0; i < carCollection.length; i++) {     
         if (carCollection[i].id === carToDeleteId) {
+            carFound = true;
             carCollection.splice(i, 1);
-            res.status(200).send('Car deleted.');
         }
     }
-    res.status(404).send('Not found');
+    if (carFound) 
+     res.status(200).send('Car deleted.');
+    else
+     res.status(404).send('Not found!');
 });
 
-router.put("/car/:id", (req, res) => {
+router.put("/api/cars/:id", (req, res) => {
     let carToEditId = parseInt(req.params.id);
+    let carFound = false;
   
     for (let i = 0; i < carCollection.length; i++) {
         if (carCollection[i].id === carToEditId) {
-            carCollection[i].name = req.body.name;
-            carCollection[i].hp = req.body.hp;
-            res.status(200).send('Car edited.');
-        }
+            carFound = true;
+            carCollection[i].attributes.name = req.body.name;
+            carCollection[i].attributes.hp = req.body.hp;
+        } 
     }
-    res.status(404).send('Not found');
+
+    if (carFound) 
+        res.status(200).send('Car updated.');
+    else
+        res.status(404).send('Not found!');
 });
 
 module.exports = router;
